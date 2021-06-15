9dac6092b0637a34fad3385601d348a854c8f680
Anzum Praveen <75968464+anzumpraveen@users.noreply.github.com>
2021-06-15 09:17:13 +0530

    Add files via upload

---
+++ subject.js
@@ -0,0 +1,11 @@
+var sub="hindi"
+var sub="english"
+if (sub=="english"){
+    console.log("this is english tutorial")
+}
+else if(sub=="hindi"){
+    console.log("this is hindi tutorial")
+}
+else{
+    console.log("not in subject")
+}
